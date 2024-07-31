81bd7ed5a4f43548fe5c458677e4ecdc4f90d512
unknown <ivaylodimitrov031@gmail.com>
2024-07-31 16:48:40 +0300

    some changes

--- TestProject1/TestCalculator.cs
+++ TestProject1/TestCalculator.cs
@@ -20,7 +20,16 @@ namespace TestProject1
         [OneTimeSetUp]
         public void SetUp()
         {
-            driver = new ChromeDriver();
+            var options = new ChromeOptions();
+            options.AddArguments("headless");
+            options.AddArguments("no-sandbox");
+            options.AddArguments("disable-dev-shm-usage");
+            options.AddArguments("disable-gpu");
+            options.AddArguments("window-size=1920x1080");
+            options.AddArguments("disable-extensions");
+            options.AddArguments("remote-debugging-port=9222");
+
+            driver = new ChromeDriver(options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Url = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com/number-calculator/";
 
@@ -36,6 +45,7 @@ namespace TestProject1
         public void TearDown()
         {
             driver.Quit();
+            driver.Dispose();
         }
 
         public void PerformCalculation(string firstNumber, string operation,
